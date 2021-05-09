2001163d2566138670def048f4a53543cc1d1085
Ovindu Wijethunge <oumw.udesh@gmail.com>
2021-05-09 11:35:33 +0530

    errors corrected

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='Health-Care-Application-Deployement',
-    packages=['Health-Care-Application-Deployement'],
+    name='Health_Care_Application_Deployement',
+    packages=['Health_Care_Application_Deployement'],
     include_package_data=True,
     install_requires=[
         'flask',
